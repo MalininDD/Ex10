8ceae479206637912da84bf8452c808c3f1bfb20
MalininDD <54958481+MalininDD@users.noreply.github.com>
2021-01-19 13:43:40 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@
 int main() {
     std::string s1("9 + 5 * (3.14 - 6.17) * 7");
     std::cout << s1 << std::endl;
-    std::string s_out = infix2prefix(s1);
+    std::string s_out = infix2postfix(s1);
     std::cout << s_out << std::endl;
     std::string s2("7 + 4");
     std::cout << s2 << std::endl;
-    std::string s_out2 = infix2prefix(s2);
+    std::string s_out2 = infix2postfix(s2);
     std::cout << s_out2 << std::endl;
     std::string s3("(2 + 4) * (4.5557 - 4.53434)");
     std::cout << s3 << std::endl;
-    std::string s_out3 = infix2prefix(s3);
+    std::string s_out3 = infix2postfix(s3);
     std::cout << s_out3 << std::endl;
 
     return 0;
